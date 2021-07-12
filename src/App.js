5d8ec2f6695b14334eab88e561bc5b5699b9f634
Madhav Prasad Koirala <mpk2053@gmail.com>
2021-07-11 21:43:13 -0500

    add a banner in home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -14,10 +15,9 @@ function App() {
           <Route path="/login">
             <h1>Login page</h1>
           </Route>
-          {/* Default goes at the end in the router */}
           <Route path="/">
             <Header />
-            <h1>Home Page</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home_img" src="https://fortheloveblog.com/wp-content/uploads/2016/07/Amazon-Prime-Banner.jpg">
+            </img>
+        </div>
+    )
+}
+
+export default Home
